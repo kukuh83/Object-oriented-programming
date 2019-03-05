c8a36998cf55fb194bf73ae19f245099f64b6a5e
kukuh <48233545+kukuh83@users.noreply.github.com>
2019-03-05 19:07:05 +0700

    kukuh aji santoso 17090059 4D

---
+++ kukuh_aji_santoso_17090059_4D.java
@@ -0,0 +1,10 @@
+
+package output;
+
+public class kukuh_aji_santoso_17090059_4D {
+    public static void main(String[] args) {
+        System.out.println("selamat datang di object oriented programing \n kukuh_aji_santoso_17090059_4D");
+    }
+    }
+    
+
