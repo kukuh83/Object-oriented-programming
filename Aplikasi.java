83e51c303109f4e29c9c6033f9ca7d0e9800c641
kukuh <48233545+kukuh83@users.noreply.github.com>
2019-03-26 19:54:10 +0700

    Add files via upload

---
+++ Aplikasi.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Aplikasi {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("halo");
+        frame.setSize(400, 250);
+
+        frame.setLayout(new BorderLayout());
+
+        JLabel lblnama =new JLabel();
+        lblnama.setText("Kukuh Aji Santoso");
+       // frame.add(lblnama, BorderLayout.WEST);
+        JLabel lblNim =new JLabel();
+        lblNim.setText("19090059");
+
+        JButton btnSimpan =new JButton("Simpan");
+        frame.add(btnSimpan, BorderLayout.EAST);
+
+        JPanel westPanel =new JPanel();
+        westPanel.setLayout(new GridLayout(3,1));
+        frame.add(westPanel, BorderLayout.WEST);
+        westPanel.add(lblnama);
+        westPanel.add(lblNim);
+
+
+
+        frame.setVisible(true);
+
+
+    }
+
+}
